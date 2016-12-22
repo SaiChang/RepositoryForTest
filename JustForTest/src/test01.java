9844e6a1efbbb959a25711b764a54f17990b1a92
SaiChang <932964601@qq.com>
2016-12-23 00:23:03 +0800

    the first modification

--- JustForTest/src/test01.java
+++ JustForTest/src/test01.java
@@ -2,7 +2,7 @@
 public class test01 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("the content is modified");
 	}
 	
 }
---
+++ JustForTest/src/test02.java
@@ -0,0 +1,10 @@
+
+public class test02 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is the test02");
+		
+	}
+
+}
